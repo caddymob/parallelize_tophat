9d6dfefdaa6e24e24a8f97fbf4c9724bf6cbd24a
Jason Corneveaux <jason@corneveaux.com>
2013-04-01 15:56:41 -0700

    cleaned up, still getting stuck in the queue, not sure why ... hanging up for tonight

--- startSplitReads_tophat.sh
+++ startSplitReads_tophat.sh
@@ -19,7 +19,6 @@ if [ -z ${NODE_COUNT} ]; then
 	exit
 fi
 
-
 mkdir -p jobLogs
 
 FQ1=`echo ${SAMPLE_ID}.R1.QC.fq.gz`
@@ -31,16 +30,17 @@ echo -ne "\nJOB PATH: ${JOB_PATH}\n"
 
 echo "${SAMPLE_ID} ${JOB_PATH}" > ${HOME}/SCRIPTS/parallelize_tophat/tophat2/messages/${SAMPLE_ID}.started
 
-splitJob=`qsub \
+splitJob=$(qsub \
  -v SAMPLE_ID=${SAMPLE_ID},FQ1=${FQ1},FQ2=${FQ2},JOB_PATH=${JOB_PATH},NODE_COUNT=$NODE_COUNT \
- -d ${JOB_PATH} \
  -N ${SAMPLE_ID}.Split \
- ~/SCRIPTS/parallelize_tophat/tophat2/make_split_reads.pbs | sed -e 's/.newmoab.local//g'`
+ ~/SCRIPTS/parallelize_tophat/tophat2/make_split_reads.pbs | sed -e 's/.newmoab.local//g')
 
 echo -ne "\n\nSplitting ${FQ1} and ${FQ2} started with job ${splitJob} `date`\n"
 
 crontab -l > currentcrontabs
+
 echo "* * * * * bash ${HOME}/SCRIPTS/parallelize_tophat/tophat2/tophat2_slit_cronScript.v2.sh ${SAMPLE_ID} >> ${JOB_PATH}/${SAMPLE_ID}.cron.log"  >> ${SAMPLE_ID}_cron
+
 cat currentcrontabs >> ${SAMPLE_ID}_cron
 crontab ${SAMPLE_ID}_cron
 
@@ -52,3 +52,6 @@ rm currentcrontabs
 echo -ne "\nCron job will pick up from once fastqs are done splitting, just watch the queue explode...\n\n"
 
 echo -ne "\n\n`qstat | grep ${SAMPLE_ID} | tail -1`\n\n"
+ 
+ 
+ #-d ${JOB_PATH} \ # taken out of splitJob for now...?
