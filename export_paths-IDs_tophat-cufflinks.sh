728dd8f27c92d400c1e4a227ab69dc69334390ad
Jason Corneveaux <jason@corneveaux.com>
2013-04-02 16:20:50 -0700

    it works. just updating bowtie and tophat, might as well since the update says segment mapping slow-down had been fixed. This was a major bottleneck in parallizing too

--- export_paths-IDs_tophat-cufflinks.sh
+++ export_paths-IDs_tophat-cufflinks.sh
@@ -10,22 +10,24 @@ else
 fi
 
 export PATH=$PATH:${HOME}/bin
+export DATE=`echo -n $(date '+aln_%m-%d-%y')`
 
 export BWA=~/bin/bwa
 export CUFFCOMPARE=~/bin/CUFFLINKS/2.0.2/cuffcompare
 export CUFFLINKS=~/bin/CUFFLINKS/2.0.2/cufflinks
 export CUFFDIFF=~/bin/CUFFLINKS/2.0.2/cuffdiff
-export DATE=`echo -n $(date '+aln_%m-%d-%y')`
 export GATK=~/bin/GenomeAnalysisTK-latest/GATK2/GenomeAnalysisTK.jar
+export PICARD=~/bin/picard-tools-current/
+export SAMTOOLS=~/bin/samtools
+export TOPHAT=~/bin/tophat-2.0.8.Linux_x86_64/tophat2
+
 export GTF=${REF_DIR}/GRCh37/bowtie2_b37/Homo_sapiens.GRCh37.66.gtf
 export HUMAN_REF_FA=${REF_DIR}/GRCh37/bowtie2_b37/Homo_sapiens.GRCh37.62
 export REF=${HUMAN_REF_FA}.fa
 export HUMAN_TRANS_FA=${REF_DIR}/GRCh37/bowtie2_b37/Homo_sapiens.GRCh37.transcriptome/Homo_sapiens.GRCh37.66
 export MASK=${REF_DIR}/GRCh37/bowtie2_b37/Homo_sapiens.GRCh37.66.MASK.gtf
-export PICARD=~/bin/picard-tools-current/
 export BWA_REF=${REF_DIR}/GRCh37/Homo_sapiens.GRCh37.62.fa
-export SAMTOOLS=~/bin/samtools
-export TOPHAT=~/bin/tophat-2.0.4.Linux_x86_64/tophat
 export VCF=${REF_DIR}/GRCh37/dbSNP137.vcf
+
 export THREADS=`grep -c ^processor /proc/cpuinfo`
 export ALN_THREADS=`echo $THREADS / 2 | bc`
